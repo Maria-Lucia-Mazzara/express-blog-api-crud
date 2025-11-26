dde680c9719b9962ab2b0eee1ce5a9e78ee81cbd
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-26 17:08:23 +0100

    sostituito funzione index con la funzione index però chiamata json fatta condizione nelle funioni show e destory

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,15 +1,39 @@
 
+let posts = require('../dati/posts_array');
+
 // questo codice ha il compito di preparare la risposta e mandarla al client
 
-// raccoglie tutta la lista
+// // raccoglie tutta la lista
+// function index(req, res) {
+//     res.send("Lista dei post");
+// }
+
+// restituisce l’array completo in formato JSON
 function index(req, res) {
-    res.send("Lista dei post");
+    res.json(posts);
 }
 
-// risposta con i dettagli richiesti
+// // risposta con i dettagli richiesti
+// function show(req, res) {
+//     res.send("Dettagli del post " + req.params.id);
+// }
+
 function show(req, res) {
-    res.send("Dettagli del post " + req.params.id);
+    const id = parseInt(req.params.id);
+
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        res.status(404);
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        });
+    }
+
+    res.json(post);
 }
+
 
 // risposta con un messaggio
 function store(req, res) {
@@ -22,10 +46,18 @@ function update(req, res) {
 function modify(req, res) {
     res.send("Modifica parziale del post " + req.params.id);
 }
-// qui elimino un singolo elemento
+// // qui elimino un singolo elemento
+// function destroy(req, res) {
+//     res.send("Eliminazione del post " + req.params.id);
+// };
+
 function destroy(req, res) {
-    res.send("Eliminazione del post " + req.params.id);
-};
+    const id = parseInt(req.params.id);
+
+    posts = posts.filter((post) => post.id !== id);
+    console.log("changed list", posts);
+    res.status(204).send();
+}
 
 module.exports = { index, show, store, update, modify, destroy };
 // esporto le funzioni
