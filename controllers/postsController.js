22153f44d18f8312abd569f85975c7f85979d9fd
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-26 15:52:16 +0100

    aggiunta la funzione show al documento controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,3 +4,8 @@ function index(req, res) {
     res.send("Lista dei post");
 }
 
+// risposta con i dettagli richiesti
+function show(req, res) {
+    res.send("Dettagli del post " + req.params.id);
+}
+
