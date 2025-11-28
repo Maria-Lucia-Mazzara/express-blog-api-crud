95197f94c44f1b85cb37ba26c1a8500679b33ffb
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-28 17:00:23 +0100

    importato documento del mio errore e aggiunto la riga di codice per farlo avvenire e poi ho testato

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const posts = require('./routers/posts')
 const notFound = require('./middlewares/notFound')
+const erroreMio = require('./middlewares/erroreMio')
 
 // comando per leggere il body json 
 app.use(express.json());
@@ -14,12 +15,17 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
+    // // lollo l'ho generato io per far apparire l'errore
+    // lollo
     res.send('<h1>Lista di cibo vario</h1>')
 })
 
 
 // Collego il router dei posts
 app.use('/posts', posts)
+
+// errore fatto da me
+app.use(erroreMio)
 
 // inserisco l'errore utente 
 app.use(notFound)
