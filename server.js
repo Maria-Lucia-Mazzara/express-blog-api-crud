71131f374ecd20c9ceb02e3572d315229b3b0622
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-28 16:40:36 +0100

    importato documento notFound e aggiunta riga dell'errore nel documento server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const posts = require('./routers/posts')
-
+const notFound = require('./middlewares/notFound')
 
 // comando per leggere il body json 
 app.use(express.json());
@@ -17,9 +17,12 @@ app.get('/', (req, res) => {
     res.send('<h1>Lista di cibo vario</h1>')
 })
 
+
 // Collego il router dei posts
 app.use('/posts', posts)
 
+// inserisco l'errore
+app.use(notFound)
 
 
 // Dopo aver completato tutte le operazioni CRUD, completiamo le nostre API inserendo un middleware per la gestione delle rotte non registrate e uno per la gestione degli errori.
